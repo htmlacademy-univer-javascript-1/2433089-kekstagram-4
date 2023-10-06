34f04fba3ae1e93e7a2795baa87b2e97afb5e77e
Перегуда Виктор <pereguda.vitia@mail.ru>
2023-10-06 20:35:36 +0500

    Переделал под критерии

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,10 @@
-function checkLineLength(inputStr, maxLength){
-  if (inputStr.length <= maxLength){
-    return true;
-  }
-  return false;
-}
+const checkLineLength = (input, maxLength) => input.length <= maxLength;
 
 checkLineLength('проверяемая строка', 20); // true
 checkLineLength('проверяемая строка', 18); // true
 checkLineLength('проверяемая строка', 10); // false
 
-function isPalindrom(inputStr){
-  const line = inputStr.replaceAll(' ','').toLowerCase();
-  let reversedLine = '';
-  for (let i = line.length - 1; i >= 0; i--){
-    reversedLine += line[i];
-  }
-  return reversedLine === line;
-}
+const isPalindrom = (inputStr) => (inputStr.toLowerCase().replaceAll(' ', '') === inputStr.toLowerCase().replaceAll(' ', '').reverse());
 
 isPalindrom('топот'); // true
 isPalindrom('ДовОд'); // true
