5f3d03aec501ecd3a301e567d4cbea4e2e47c299
Перегуда Виктор <pereguda.vitia@mail.ru>
2023-10-29 21:46:46 +0500

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,21 @@
-const checkLineLength = (input, maxLength) => input.length <= maxLength;
+const getMinutes = (time) => {
+  const [hours, minutes] = time.split(':').map(Number);
+  return hours * 60 + minutes;
+};
 
-checkLineLength('проверяемая строка', 20); // true
-checkLineLength('проверяемая строка', 18); // true
-checkLineLength('проверяемая строка', 10); // false
+const checkTime = (startWorkDay, endWorkDay, startMeeting, meetingTimeValue) => {
+  const startWorkDayMinutes = getMinutes(startWorkDay);
+  const endWorkDayMinutes = getMinutes(endWorkDay);
+  const startMeetingMinutes = getMinutes(startMeeting);
 
-const isPalindrom = (inputStr) => (inputStr.toLowerCase().replaceAll(' ', '') === inputStr.toLowerCase().replaceAll(' ', '').reverse());
+  return (
+    startMeetingMinutes >= startWorkDayMinutes &&
+    startMeetingMinutes + meetingTimeValue <= endWorkDayMinutes
+  );
+};
 
-isPalindrom('топот'); // true
-isPalindrom('ДовОд'); // true
-isPalindrom('Кекс');  // false
-isPalindrom('Лёша на полке клопа нашёл '); // true
+checkTime('08:00', '17:30', '14:00', 90); // true
+checkTime('8:0', '10:0', '8:0', 120);     // true
+checkTime('08:00', '14:30', '14:00', 90); // false
+checkTime('14:00', '17:30', '08:0', 90);  // false
+checkTime('8:00', '17:30', '08:00', 900); // false
